73f021a3d39aebbe80c1768511fa406d00a727ff
Alberto González <albertogonzalez@acidtango.com>
2024-05-24 16:00:26 +0100

    green: coffee made

--- src/CoffeeMachine.ts
+++ src/CoffeeMachine.ts
@@ -1,15 +1,24 @@
 import { DrinkMaker } from "./DrinkMaker.js"
 
 export class CoffeeMachine {
+  private selectedDrink = ""
+
   constructor(private readonly drinkMaker: DrinkMaker) {}
 
   selectTea() {}
-  selectChocolate() {}
-  selectCoffee() {}
+
+  selectChocolate() {
+    this.selectedDrink = "H::"
+  }
+
+  selectCoffee() {
+    this.selectedDrink = "C::"
+  }
+
   addSugar() {}
   addStick() {}
 
   makeDrink() {
-    this.drinkMaker.execute("H::")
+    this.drinkMaker.execute(this.selectedDrink)
   }
 }
