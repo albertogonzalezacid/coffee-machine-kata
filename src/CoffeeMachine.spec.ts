9345985bd740854032cce453c5aa092a5cbd782c
Alberto González <albertogonzalez@acidtango.com>
2024-05-24 16:00:15 +0100

    red: makes coffee

--- src/CoffeeMachine.spec.ts
+++ src/CoffeeMachine.spec.ts
@@ -25,4 +25,14 @@ describe("CoffeeMachine", () => {
 
     expect(drinkMaker.instructions).toBe("H::")
   })
+
+  it("makes 1 coffee with no sugar and therefore no stick", () => {
+    const drinkMaker = new DrinkMakerFake()
+    const coffeeMachine = new CoffeeMachine(drinkMaker)
+
+    coffeeMachine.selectCoffee()
+    coffeeMachine.makeDrink()
+
+    expect(drinkMaker.instructions).toBe("C::")
+  })
 })
